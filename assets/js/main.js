d0fbdb6292f120f226826aac043351cc6da0ceb5
Miguel Arcanjo <miguelarcanjodap@gmail.com>
2026-05-17 23:57:01 -0300

    Primeira alteração

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+function RecebeEventoForm(evento) {
+  evento.preventDefault();
+  const style = document.querySelector(".style")
+  const Form = document.querySelector(".form");
+  const Peso = document.querySelector(".input-valor");
+  const Resultado = document.querySelector(".resultado");
+  const Altura = document.querySelector(".input-valor1");
+  let calcula = Number(Peso.value) / Number(Altura.value) ** Number(Altura.value);
+
+    let grau = ["Abaixo do peso", "Normal", "Sobrepeso", "Obesidade nivel 1", "Obesidade nivel 2", "Obesidade nivel 3"]
+
+    let grauDefinitivo
+   
+    if (calcula <= 18.5){
+    grauDefinitivo = grau[0]
+    }
+    else if (calcula > 18.5 && calcula <= 24.9){
+    grauDefinitivo = grau[1]
+    } 
+    else if (calcula > 24.9 && calcula <= 29.9){
+    grauDefinitivo = grau[2]
+    }
+    else if (calcula > 29.9 && calcula <= 34.9){
+    grauDefinitivo = grau[3]
+    }
+    else if (calcula > 34.9 && calcula <= 39.9){
+    grauDefinitivo = grau[4]
+    }
+    else{
+      grauDefinitivo = grau[5]
+    }
+  
+    if (Peso.value && Altura.value !== "" && calcula) {
+    Resultado.innerHTML = `Seu IMC é igual a ${calcula.toFixed(2)} e você está classificado em ${grauDefinitivo}`;
+  }
+  else{
+    Resultado.innerHTML = "Algo está errado"
+  }
+
+  
+}
+addEventListener("submit", RecebeEventoForm);
